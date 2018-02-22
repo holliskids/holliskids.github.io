fedc6ca94ef9b6798a95bd05c4c6a577da1cccdc
holliskids <35231825+holliskids@users.noreply.github.com>
2018-02-22 13:38:34 -0700

    Flexbox And Grid Added

---
+++ assignments/franklin-7/scripts/windchill.js
@@ -0,0 +1,13 @@
+function windChill()
+{
+var high = parseFloat(document.getElementById('high').textContent);
+var low = parseFloat(document.getElementById('low').textContent);
+var temp = (high+low)/2;
+
+var windSpeed = parseFloat(document.getElementById('speed').textContent);
+
+var wChillFactor = 35.74 + 0.6215*temp - 35.75*Math.pow(windSpeed, 0.16) + 0.4275*temp*Math.pow(windSpeed, 0.16);
+wChillFactor = Math.round(wChillFactor);
+
+document.getElementById("chill").innerHTML = wChillFactor;
+}
