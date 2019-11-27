a5cf2644c5f06824ef0bc1cb49364ec86ee35394
holliskids <35231825+holliskids@users.noreply.github.com>
2019-11-26 21:25:24 -0700

    Fixed Occasion Page

---
+++ rosehillBlooms/Backup/displayPics.js
@@ -0,0 +1,45 @@
+var myArticle = document.querySelector('article');
+
+var requestURL = 'data/arrangementPics2.json';
+console.log(requestURL);
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var pictureInfo = request.response;
+    console.log(pictureInfo);
+    storePictureInfo(pictureInfo);
+}
+
+function storePictureInfo(jsonObj) {
+    var picName = jsonObj['pictures'];
+
+    for (var i = 0; i < picName.length; i++)
+    {
+        var myAnchor = document.createElement("A");
+        myAnchor.target = "_blank";
+        myAnchor.href = picName[i].fileName;
+
+        var myImg = document.createElement('img');
+        myImg.src = picName[i].reference;
+        myImg.alt = "Floral Picture";
+
+        myAnchor.appendChild(myImg);
+        myArticle.appendChild(myAnchor);
+    }
+}
+
+
+
+/*<script>
+function myFunction() {
+  var x = document.createElement("A");
+  var t = document.createTextNode("Tutorials");
+  x.setAttribute("href", "https://www.w3schools.com");
+  x.appendChild(t);
+  document.body.appendChild(x);
+}
+</script>
+*/
--- rosehillBlooms/scripts/displayOccasion.js
+++ rosehillBlooms/scripts/displayOccasion.js
@@ -1,4 +1,4 @@
-var myArticle = document.querySelector('article');
+var currentDivision = document.getElementById("column1");
 
 var requestURL = 'data/occasion.json';
 console.log(requestURL);
@@ -13,21 +13,33 @@ request.onload = function() {
     storePictureInfo(pictureInfo);
 }
 
-function storePictureInfo(jsonObj) {
-    var picName = jsonObj['pictures'];
+function createHtmlPics(index, length, jsonObj)
+{   var namePic = jsonObj['pictures'];
 
-    for (var i = 0; i < picName.length; i++)
+    for (var x = index; x < length; x++)
     {
         var myAnchor = document.createElement("A");
-        myAnchor.target = "_blank";
-        myAnchor.href = picName[i].fileName;
+        myAnchor.target= "_blank";
+        myAnchor.href = namePic[x].fileName;
 
         var myImg = document.createElement('img');
-        myImg.src = picName[i].reference;
+        myImg.src = namePic[x].fileName;
         myImg.alt = "Floral Picture";
 
         myAnchor.appendChild(myImg);
-        myArticle.appendChild(myAnchor);
+        currentDivision.appendChild(myAnchor);
     }
+    return x;
+}
+
+function storePictureInfo(jsonObj) {
+    var picName = jsonObj['pictures'];
+    var numInColumn = Math.trunc(picName.length/3);
+
+    var newIndex = createHtmlPics(0, numInColumn, jsonObj);
+    currentDivision = document.getElementById("column2");
+    newIndex = createHtmlPics(newIndex, numInColumn+newIndex+1, jsonObj);
+    currentDivision = document.getElementById("column3");
+    newIndex = createHtmlPics(newIndex, picName.length, jsonObj);
 }
 
